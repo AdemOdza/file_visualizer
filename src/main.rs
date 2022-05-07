35b60690c042e9d249c4a93ed4b5a13624cb471e
AdemOdza <Albania1veleshta!>
2022-05-07 12:53:38 -0500

    Started work on printing logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::{env, fs};
 use std::fs::metadata;
 use std::path::Path;
+use std::io::stdout;
+use std::io::Write;
+
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
@@ -24,18 +27,36 @@ fn main() {
 
 fn display_tree(filepath: &Path, depth: i32){
     let file_info = metadata(filepath).unwrap();
+    //Get contents of dir
+    //iterate through each item in dir
+    let children = fs::read_dir(filepath).unwrap();
 
-    if file_info.is_file(){
-        let name = filepath.file_name().expect("Error reading OsStr filename");
-        println!("{}", name.to_str().expect("Error reading filename"));
-        todo!("Add");
 
-    }
-    else {
-
+    for child in children {
+        let curr_file = child.expect("Error finding child directory");
+        let file_info = curr_file.metadata().unwrap();
+        
+        if file_info.is_file(){
+            print!(" ");
+            for i in 1..depth-1 {
+                print!("   │");
+            }
+            let name = filepath.file_name().expect("Error reading OsStr filename");
+            println!("├───{}", name.to_str().expect("Error reading filename"));
+            todo!("Add");
+    
+        }
+        else {
+            todo!("Add print logic for directories");
+            display_tree(curr_file.path().as_path(), depth + 1);
+        }
     }
     //If file is last, print L
     // else, print |-
     //IF its a file, print - Name
+
+    // ├ ─ ─ ─ 
+    // └ ─ ─ ─
+    // │
 
 }
